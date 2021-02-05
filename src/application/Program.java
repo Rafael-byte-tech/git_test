8bb8fca701edfd5a315317596638d9b9321f5f43
Rafael Machado <rafaelmachadosouza3@gmail.com>
2021-02-05 20:08:18 -0300

    mensagem de boa noite e matriz

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
-
+import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
+		Integer n = 3, m = 3;
+		int vect [] [] = new int [n] [m];
+		
+		for(Integer i = 0; i < n; i ++) {
+			for(Integer j = 0; j < m; j ++) {
+				vect [i] [j] = sc.nextInt();
+			}
+		}
+		
+		
+		
 	}
 
 }
